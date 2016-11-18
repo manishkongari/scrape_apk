0814f562735c77d1ebcc99c8feb7a423131d5361
Manish Kongari <manishkongari@outlook.com>
2016-11-18 07:12:00 +0530

    added create csv

--- app/controllers/homes_controller.rb
+++ app/controllers/homes_controller.rb
@@ -1,10 +1,28 @@
 class HomesController < ApplicationController
+  require 'nokogiri'
+  require 'open-uri'
+
   def index
-    require 'nokogiri'
-    require 'open-uri'
     url = "https://apkpure.co/"
     @doc = Nokogiri::HTML(open(url))
-    @xml = Nokogiri::XML(open(url+ 'sitemap.xml'))
+    xml_file = open(url+ 'sitemap.xml')
+    @xml = Nokogiri::HTML(xml_file)
+  end
 
+  def download_csv
+    require 'csv'
+    url = "https://apkpure.co/"
+    xml_file = open(url+ 'sitemap.xml')
+    xml = Nokogiri::HTML(xml_file)
+    csv_string = CSV.generate do |csv|
+      csv << ["link"]
+      xml.css("loc").each do |item|
+        csv << [item.text]
+      end
+    end
+
+    respond_to do |format|
+      format.csv { send_data csv_string, filename: "sitemap-links.csv" }
+    end
   end
 end
